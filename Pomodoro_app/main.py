c20b9ec8a8ecc92d596884f375e8131a306b837d
KoRnWolF <pinkcrawler@gmail.com>
2024-11-19 21:50:27 +0200

    Update Main
    UI completed
    Partial counter implemented - Fix count issues and overwrite

--- Pomodoro_app/main.py
+++ Pomodoro_app/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#c62e2e"
@@ -10,10 +11,24 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    # canvas.delete(counter_text)
+    canvas.create_text(100, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
+    final_countdown()
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def final_countdown(workmin=25):
+    mins, secs = divmod(workmin, 60)
+    print(workmin)
+    print(mins)
+    canvas.itemconfig(counter_text, text=workmin)
+    if workmin > 0:
+        canvas.after(1000, final_countdown, workmin - 1)
+    print(workmin)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,10 +37,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.grid(column = 1, row = 1)
+counter_text = canvas.create_text(100,130, text="00:00", fill = "white", font=(FONT_NAME, 35, "bold"))
 
-canvas.create_text(100,130, text="00:00", fill = GREEN, font=(FONT_NAME, 35, "bold") )
+start_button = Button(text="START", bg="green", fg="white" , command=start_timer)
+start_button.grid(column = 0, row = 2)
 
+reset_button = Button(text="RESET", bg="green", fg="white", command=reset_timer)
+reset_button.grid(column = 2, row = 2)
 
+header_label = Label(text="TIMER", fg = GREEN, bg= YELLOW, font=(FONT_NAME, 35, "bold"))
+header_label.grid(column = 1, row = 0)
+
+check_label = Label(text="✅︎",bg = YELLOW, fg = GREEN)
+check_label.grid(column = 1, row = 3)
 
 window.mainloop()
