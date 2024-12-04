890693262c6c14a8ce0cebefa4878e559e9c7358
KoRnWolF <pinkcrawler@gmail.com>
2024-12-04 21:07:00 +0200

    Complete last commit
    added checks
    added reset
    added reps

--- Pomodoro_app/main.py
+++ Pomodoro_app/main.py
@@ -9,23 +9,68 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = "none"
+timer_running = False
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset_timer():
-    pass
+    global timer
+    global timer_running
+    timer_running = False
+    window.after_cancel(timer)
+    canvas.itemconfig(counter_text, text=f"00:00")
+    header_label.config(text="TIMER", fg = GREEN, bg= YELLOW, font=(FONT_NAME, 35, "bold"))
+    check_label.config(text="")
+    global reps
+    reps = 0
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    final_countdown(WORK_MIN * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if not timer_running: #check if timer is running
+        if reps == 1 or reps == 3 or reps == 5 or reps == 7:
+            header_label.config(text="WORK SLAVE")
+            final_countdown(work_sec)
+        if reps == 8:
+            header_label.config(text="LONG BREAK", fg=PINK)
+            window.attributes("-topmost", True)
+            window.attributes("-topmost", False)
+            final_countdown(long_break_sec)
+        if reps == 2 or reps == 4 or reps == 6:
+            header_label.config(text="SHORT BREAK", fg=RED)
+            window.attributes("-topmost", True)
+            window.attributes("-topmost", False)
+            final_countdown(short_break_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def final_countdown(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
-    mins, secs = divmod(count, 60)
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
     canvas.delete("counter")
     canvas.itemconfig(counter_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, final_countdown, count - 1)
+        global timer
+        global timer_running
+        timer_running = True
+        timer = window.after(1000, final_countdown, count - 1)
+    if count == 0:
+        timer_running = False
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✅︎"
+        check_label.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -46,7 +91,7 @@ reset_button.grid(column = 2, row = 2)
 header_label = Label(text="TIMER", fg = GREEN, bg= YELLOW, font=(FONT_NAME, 35, "bold"))
 header_label.grid(column = 1, row = 0)
 
-check_label = Label(text="✅︎",bg = YELLOW, fg = GREEN)
+check_label = Label(bg = YELLOW, fg = GREEN)
 check_label.grid(column = 1, row = 3)
 
 window.mainloop()
