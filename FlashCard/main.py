bc42d9e8fbb039b16e956b3a3db8561d4961016f
KoRnWolF <pinkcrawler@gmail.com>
2025-02-03 22:16:06 +0200

    Flashcard update
    added French and button functions

--- FlashCard/main.py
+++ FlashCard/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-
+import random
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -8,23 +9,35 @@ window = Tk()
 window.title("Flasher")
 window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
 
+def pull_word():
+    random_num = random.randint(0,len(df_list))
+    # english_word = (df_list[2]['English'])
+    canvas.itemconfig(title, text="French")
+    french_word = (df_list[random_num]['French'])
+    canvas.itemconfig(word, text=french_word)
+
+df = pandas.read_csv("data/french_words.csv")
+df_list = df.to_dict('records')
+first_random_num = random.randint(0, len(df_list))
+first_french_word = (df_list[first_random_num]['French'])
+
 #Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card_front = PhotoImage(file="e:/pycharm/Projects/FlashCard/images/card_front.png")
+card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,270, image=card_front)
 canvas.grid(column=0, row=0,columnspan=2)
 
 #Canvas Text
-title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+title = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text=first_french_word, font=("Ariel", 60, "bold"))
 
 #Canvas Buttons
-right_image = PhotoImage(file="e:/pycharm/Projects/FlashCard/images/right.png")
-right_btn = Button(image=right_image, highlightthickness=0)
+right_image = PhotoImage(file="images/right.png")
+right_btn = Button(image=right_image, highlightthickness=0, command = pull_word)
 right_btn.grid(column=1, row=1)
 
-wrong_image = PhotoImage(file="e:/pycharm/Projects/FlashCard/images/wrong.png")
-wrong_btn = Button(image=wrong_image, highlightthickness=0)
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_btn = Button(image=wrong_image, highlightthickness=0, command = pull_word)
 wrong_btn.grid(column=0, row=1)
 
 
